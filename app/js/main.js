651c2b407697fcf6e09964deba4729921feee0a2
daria-dudka <dasha-s@ukr.net>
2020-07-10 19:04:40 +0300

    added slider section

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $('.rate-star').rateYo({
+    rating: 4.5,
+    starWidth: "16px",
+    readOnly: true
+  });
+
+  $('.slider__inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt=""></button>'
+  });
+
+  
+  
+});
