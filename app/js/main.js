5169465b12b181e47a09ae76d04beafb826c4f7c
daria-dudka <dasha-s@ukr.net>
2020-07-17 17:06:17 +0300

    added followers-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,14 @@ $(function(){
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt=""></button>'
   });
 
+  $('.followers-slider__inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.png" alt=""></button>',
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+
   var mixer = mixitup('.newest__inner-box');
   
 });
